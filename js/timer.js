bd6f785ab001c620b9bf6ce1a33f16e5da14aae8
jinyeong-jung <jinyeong32@gmail.com>
2018-11-14 12:02:31 +0900

    Finish timer feature
    Delete saving time & history feature

--- js/timer.js
+++ js/timer.js
@@ -2,10 +2,6 @@ const timerForm = document.querySelector(".timer_form");
 const timerText = document.querySelector(".timer_clock");
 const startBtn = document.querySelector(".timer_btn");
 const stopBtn = document.querySelector(".timer_btn_stop");
-const saveBtn = document.querySelector(".timer_btn_save");
-const historyBtn = document.querySelector(".timer_history_btn");
-const historyWrap = document.querySelector(".timer_history_wrap"),
-    historyExitBtn = historyWrap.querySelector(".timer_history_exit");
 
 function startTimer(e) {
     // change time
@@ -32,43 +28,27 @@ function startTimer(e) {
     // stopTimer
     stopBtn.addEventListener("click", function(e){
 
+        // change time
+        clearInterval(addTime);
+
         // change button
         // stopBtn
         stopBtn.classList.add("hidden");
-        // saveBtn
-        saveBtn.classList.remove("hidden");
-        saveBtn.addEventListener("click", function(e){
-            console.log(sec, min, hour);
-            e.preventDefault();
-        });
         // startBtn
         startBtn.classList.remove("hidden");
+        startBtn.innerHTML = "RESET";
         startBtn.addEventListener("click", function(){
             setInterval(addTime);
-            saveBtn.classList.add("hidden");
         });
 
-        // change time
-        clearInterval(addTime);
         e.preventDefault();
     });
 
     e.preventDefault();
 }
 
-function hideHistory() {
-    historyWrap.classList.add("hidden");
-}
-
-function openHistory(e) {
-    historyWrap.classList.remove("hidden");
-    historyExitBtn.addEventListener("click", hideHistory);
-    e.preventDefault();
-}
-
 function init() {
     startBtn.addEventListener("click", startTimer);
-    historyBtn.addEventListener("click", openHistory);
 }
 
 init();
